2fc0d2c3316fc7dd2d3f1c95dcca43ac2c5c6d37
Fong <fong.fan.90@gmail.com>
2015-05-02 12:42:56 -0500

    Updating commit with controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  root to: 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
