80bdb5f75900c7e6b6977724469c0c8eea7ac36b
Fong <fong.fan.90@gmail.com>
2015-05-17 23:48:05 -0500

    lesson32 is complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150515234349) do
+ActiveRecord::Schema.define(version: 20150517224407) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -44,7 +44,10 @@ ActiveRecord::Schema.define(version: 20150515234349) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
 require 'faker'
 
+# Create Users
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+
 # Create Posts
 50.times do 
   Post.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
   )
@@ -12,33 +26,30 @@ posts = Post.all
 # Create Comments
 100.times do
   Comment.create!(
+    # users.sample,   #we have not yet associated Users with Comments
     post: posts.sample,
     body: Faker::Lorem.paragraph
   ) 
 end
 
-if Post.where(:title => "**This is my unique title**").length == 0 
-  Post.create!(
-      title: "**This is my unique title**",
-      body: "**This is my unique body**"
-    )
-end
 
-if Comment.where(:body => "**My unique comment on this post!**").length ==0
-  Comment.create!(
-      post: posts.sample,
-      body: "**My unique comment on this post!**"
-    )
-end
-
+# Creating my own user with the seed
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  name: 'Fong Fan',
+  email: 'fong.fan.90@gmail.com',
+  password: 'helloworld'
+)
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 
-Post.all.each do |p|
-  puts "Title: #{p.title}"
-  p.comments.all.each do |c|
-    puts "   Comments: #{c.body}"
-  end
-end
+# Post.all.each do |p|
+#   puts "Title: #{p.title}"
+#   p.comments.all.each do |c|
+#     puts "   Comments: #{c.body}"
+#   end
+# end
