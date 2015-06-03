8d1fb1ad5c014694d092b7700eb2fb1b245bc8ff
Fong <fong.fan.90@gmail.com>
2015-06-03 06:04:01 -0500

    Lesson 43 Post Markdown Assignment complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,22 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20}, presence: true
   #validates :topic, presence: true
   validates :user, presence: true
+
+  def markdown_title
+    render_as_markdown(title)
+  end
+
+  def markdown_body
+    render_as_markdown(body)
+  end
+
+  private
+
+  def render_as_markdown(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
+
 end
