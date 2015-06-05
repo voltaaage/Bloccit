76dec4fa75381d670f0187d4cf076f094da80f74
Fong <fong.fan.90@gmail.com>
2015-06-05 00:46:20 -0500

    lesson 44 uploading images complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,8 @@ class Post < ActiveRecord::Base
   #validates :topic, presence: true
   validates :user, presence: true
 
+  mount_uploader :image, ImageUploader
+
   def markdown_title
     render_as_markdown(title)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603110805) do
+ActiveRecord::Schema.define(version: 20150605045409) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20150603110805) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.string   "image"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
