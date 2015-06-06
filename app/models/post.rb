bb3e4028a21e1d06ca48db65549904a5736c7b3e
Fong <fong.fan.90@gmail.com>
2015-06-06 03:31:45 -0500

    Removing unecessary comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,6 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20}, presence: true
-  #validates :topic, presence: true
   validates :user, presence: true
 
   def markdown_title
