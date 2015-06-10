80e1b650eeee077bfd24326ee72f530d6d258ed2
Fong <fong.fan.90@gmail.com>
2015-06-09 20:26:38 -0500

    stuck2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
   def create
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     @comment = Comment.current_user.comments.build(comment_params)
     authorize @comment
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     @comment = Comment.new() 
     authorize @comment
