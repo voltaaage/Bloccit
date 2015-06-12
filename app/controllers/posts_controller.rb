16de0bf293b67523a1cc0cb6321bd96763bd01b3
Fong <fong.fan.90@gmail.com>
2015-06-11 21:33:46 -0500

    new posts did not have a topic_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,7 @@ class PostsController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(post_params)
+    @post.topic = @topic
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved."
