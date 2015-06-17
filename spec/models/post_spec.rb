eda715d7c12d0475c3f5d730a3e98ab143b86023
Fong <fong.fan.90@gmail.com>
2015-06-17 00:57:03 -0500

    lesson 48 - rspec CP complete

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe Post do
+  describe "vote methods" do
+
+    before do
+      @post = Post.create(title: 'post title', body: 'post body')
+      3.times { @post.votes.create(value: 1)}
+      2.times{ @post.votes.create(value: -1)}
+    end
+
+    describe '#up_votes' do
+      it "counts the number of votes with value = 1" do
+        expect( @post.up_votes).to eq(3)
+      end
+    end
+
+    describe '#down_votes' do
+      it "counts the number of votes with value = -1" do
+        expect( @post.down_votes).to eq(2)
+      end
+    end
+
+    describe '#points' do
+      it "returns the sum of all down and up votes" do
+        expect( @post.points ).to eq(1) # 3-2
+      end
+    end
+
+  end
+  
+end
