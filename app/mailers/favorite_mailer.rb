9035bd0931210adde973344e644793c82320fb86
Fong <fong.fan.90@gmail.com>
2015-06-29 00:47:06 -0500

    lesson 51 complete

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,5 +1,5 @@
 class FavoriteMailer < ApplicationMailer
-  default from: "youremail@email.com"
+  default from: "fong.fan.90@gmail.com"
 
   def new_comment(user, post, comment)
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,32 @@ describe User do
 
   include TestFactories
 
+  
+
   describe "#favorited(post)" do
-    xit "returns 'nil' if the user has not favorited the post" do
+    before do
+      @post = associated_post
+      @user = authenticated_user
     end
 
-    xit "returns the appropriate favorite is it exists" do
+    it "returns 'nil' if the user has not favorited the post" do
+      expect(@user.favorited(@post)).to be_nil
+    end
+
+    it "returns the appropriate favorite if it exists" do
+      # create a new favorite with the post/user from test factories
+      favorite = Favorite.create(post: @post, user: @user)
+
+      expect(@user.favorited(@post)).to eq(favorite)
     end  
+
+    it "returns nil if the user has favorited a different post than the post being passed into 'favorited'" do
+      # create a new post and a favorite
+      @post2 = associated_post(title: "new title")
+      favorite = Favorite.create(post: @post2, user: @user)
+
+      # expecting favorited to return nil when passed in the first post
+      expect(@user.favorited(@post)).to be_nil
+    end
+  end
 end
