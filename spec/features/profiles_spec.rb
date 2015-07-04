8d80a0ed4452e20f2b807e3f4cd4d6455192f6e1
Fong <fong.fan.90@gmail.com>
2015-07-04 01:28:20 -0500

    lesson 54 assignment complete

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -17,12 +17,26 @@ describe "Visiting profiles" do
     it "shows profile" do
       visit user_path(@user)
       expect(current_path).to eq(user_path(@user))
-
       expect( page ).to have_content(@user.name)
       expect( page ).to have_content(@post.title)
       expect( page ).to have_content(@comment.body)
     end
-
   end
+
+  describe "signed in" do
+      before do
+        #user sign in using Warden
+        login_as(@user, scope: :user)
+      end
+
+      it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
+  end
+
 end
 
