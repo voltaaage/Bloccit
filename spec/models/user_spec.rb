c76c1e73b477125f1f23b357d12dfd7dbf5aebef
Fong <fong.fan.90@gmail.com>
2015-07-05 20:42:24 -0500

    lesson 56 assignment complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,7 +34,9 @@ describe User do
   describe ".top_rated" do
 
     before do
-      @user1 = create(:user_with_post_and_comment)
+      @user1 = create(:user)
+      post = create(:post, user: @user1)
+      create(:comment, user: @user1, post: post)
 
       @user2 = create(:user)
       post = create(:post, user: @user2)
