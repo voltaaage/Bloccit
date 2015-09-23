cfbeb569a9be7d988dc45b9b0f045ec4ab8f896c
Fong <fong.fan.90@gmail.com>
2015-09-22 23:05:55 -0500

    reorganizing routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'comments/create'
+  root to: 'welcome#index'
+  get 'about' => 'welcome#about'
+  get 'contact' => 'welcome#contact'
 
   devise_for :users
     resources :users, only: [:update, :show, :index]
@@ -15,12 +17,5 @@ Rails.application.routes.draw do
     post '/up-vote' => 'votes#up_vote', as: :up_vote
     post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
-  
-
-  get 'about' => 'welcome#about'
-  get 'contact' => 'welcome#contact'
-  
-  root to: 'welcome#index'
-
 
 end
